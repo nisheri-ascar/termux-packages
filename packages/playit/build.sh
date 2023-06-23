4f24523d8762bda18721efca054c0a7b2fbd4343
Kristofer Racsa <50684527+nisheri-ascar@users.noreply.github.com>
2023-06-23 17:58:04 +0800

    Update build.sh

--- packages/playit/build.sh
+++ packages/playit/build.sh
@@ -1,6 +1,11 @@
 TERMUX_PKG_HOMEPAGE="https://playit.gg"
 TERMUX_PKG_DESCRIPTION="playit.gg is a global proxy that allows anyone to host a server without port forwarding."
-TERMUX_PKG_LICENSE="BSD 2-Clause License"
+TERMUX_PKG_LICENSE=TERMUX_PKG_HOMEPAGE="https://playit.gg"
+TERMUX_PKG_DESCRIPTION="playit.gg is a global proxy that allows anyone to host a server without port forwarding."
+TERMUX_PKG_LICENSE=BSD-2-Clause
+TERMUX_PKG_MAINTAINER="@nisheri-ascar"
+TERMUX_PKG_VERSION=0.9.3
+TERMUX_PKG_SRCURL=https://github.com/playit-cloud/playit-agent/archive/refs/tags/v0.9.3.tar.gz
 TERMUX_PKG_MAINTAINER="@nisheri-ascar"
 TERMUX_PKG_VERSION=0.9.3
 TERMUX_PKG_SRCURL=https://github.com/playit-cloud/playit-agent/archive/refs/tags/v0.9.3.tar.gz
