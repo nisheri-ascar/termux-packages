d6bf8a6076c52b8a4c12734378e5f7b1e3e15fad
Kristofer Racsa <50684527+nisheri-ascar@users.noreply.github.com>
2023-06-25 20:08:54 +0800

    Update build.sh

--- packages/playit/build.sh
+++ packages/playit/build.sh
@@ -9,8 +9,5 @@ TERMUX_PKG_SHA256=541a6803504b46677ee3b47e5e71bb78ded30c54e132581a10e780f67a6244
 #termux_pkg_build_in_src=true
 TERMUX_PKG_BUILD_IN_SRC=true
 TERMUX_PKG_BLACKLISTED_ARCHES="x86_64"
-termux_step_make() {
-	termux_setup_rust
-	cargo build --jobs $TERMUX_MAKE_PROCESSES --target $CARGO_TARGET_NAME --release
-}
+
 
